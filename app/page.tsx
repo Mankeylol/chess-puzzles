8570150ed15da868fcffec2694f272a66d5daff1
mankeylol <mankeyooooo@gmail.com>
2025-09-18 22:46:16 +0530

    edited homepage

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function App() {
     const response = await fetch(`/api/data?fid=${fid}`);
     const data = await response.json();
     console.log(data);
-    setScore(data.score);
+    setScore(data.highScore);
   };
 
   useEffect(() => {
@@ -44,16 +44,16 @@ export default function App() {
 
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen p-4">
-      {/* <div className="w-full max-w-[90vmin] aspect-square">
-      <Board/>
-      </div> */}
-      <div className="flex gap-6">
-      <p> Hello {username}</p>
-      <img src={context?.user?.pfpUrl} alt="hello" className="w-16 h-16 rounded-full" />
+      <div className="flex flex-col items-center gap-3">
+      <img src={context?.user?.pfpUrl || "hero.png" }  alt="hello" className="w-16 h-16 rounded-full border border-[#4B824B]" />
+      <div className="flex flex-col items-center bg-[#1E1E1E] p-1 w-32 rounded-[12px]">
+      <p className="text-[12px] text-[#4B824B]"> Hello {username}!</p>
       </div>
-      <p>Score: {score}</p>
-      <div className="flex justify-center mt-10">
-        <button onClick={() => router.push("/puzzles")} className=" bg-[#4b824b] text-white px-4 py-2 rounded">Play</button>
+      </div>
+      <p className="text-[10px] text-[#EBECD0] mt-10">HighScore: {score}</p>
+      <div className="flex justify-center mt-2 gap-2">
+        <button className=" bg-[#4b824b] text-white px-4 py-2 rounded w-32">Check Stats</button>
+        <button onClick={() => router.push("/puzzles")} className=" bg-[#4b824b] text-white px-4 py-2 rounded w-32">Play</button>
       </div>
       <Navbar/>
     </div>
